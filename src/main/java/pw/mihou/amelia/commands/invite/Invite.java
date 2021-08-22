4c21c2e3311d0f38ab22a0d5343f3459cf31984a
Mihou <kokocrunchsh@gmail.com>
2021-08-22 12:39:16 +0800

    Update invite URL.
    Signed-off-by: Mihou <kokocrunchsh@gmail.com>

--- src/main/java/pw/mihou/amelia/commands/invite/Invite.java
+++ src/main/java/pw/mihou/amelia/commands/invite/Invite.java
@@ -3,7 +3,6 @@ package pw.mihou.amelia.commands.invite;
 import org.javacord.api.entity.message.Message;
 import org.javacord.api.entity.message.MessageBuilder;
 import org.javacord.api.entity.message.component.Button;
-import org.javacord.api.entity.permission.Permissions;
 import org.javacord.api.entity.user.User;
 import org.javacord.api.event.message.MessageCreateEvent;
 import pw.mihou.amelia.templates.Embed;
@@ -17,8 +16,8 @@ public class Invite implements VelenEvent {
                 .setThumbnail(event.getApi().getYourself().getAvatar())
                 .setDescription("You can invite me freely by pressing the button below!").build())
                 .replyTo(message)
-                .addActionRow(Button.link(event.getApi().createBotInvite(Permissions.fromBitmask(67488832)), "Invite me",
-                        "\uD83D\uDC9D"))
+                .addActionRow(Button.link("https://discord.com/api/oauth2/authorize?client_id=786464598835986483&permissions=67488832&scope=bot%20applications.commands",
+                        "Invite me", "\uD83D\uDC9D"))
                 .send(event.getChannel());
     }
 
