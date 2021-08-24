725fc64d5f07a580c378bfb95641b5c792e8463a
Revi <81115039+LittleCrowRevi@users.noreply.github.com>
2021-08-24 02:49:44 +0200

    add slash command support for Invite, Ping and Test

--- src/main/java/pw/mihou/amelia/commands/invite/Invite.java
+++ src/main/java/pw/mihou/amelia/commands/invite/Invite.java
@@ -4,11 +4,19 @@ import org.javacord.api.entity.message.Message;
 import org.javacord.api.entity.message.MessageBuilder;
 import org.javacord.api.entity.message.component.Button;
 import org.javacord.api.entity.user.User;
+import org.javacord.api.event.interaction.SlashCommandCreateEvent;
 import org.javacord.api.event.message.MessageCreateEvent;
+import org.javacord.api.interaction.SlashCommandInteraction;
+import org.javacord.api.interaction.SlashCommandInteractionOption;
+import org.javacord.api.interaction.callback.InteractionImmediateResponseBuilder;
 import pw.mihou.amelia.templates.Embed;
+import pw.mihou.velen.interfaces.VelenArguments;
 import pw.mihou.velen.interfaces.VelenEvent;
+import pw.mihou.velen.interfaces.VelenSlashEvent;
 
-public class Invite implements VelenEvent {
+import java.util.List;
+
+public class Invite implements VelenEvent, VelenSlashEvent {
 
     @Override
     public void onEvent(MessageCreateEvent event, Message message, User user, String[] args) {
@@ -21,4 +29,13 @@ public class Invite implements VelenEvent {
                 .send(event.getChannel());
     }
 
+    @Override
+    public void onEvent(SlashCommandCreateEvent slashCommandCreateEvent, SlashCommandInteraction slashCommandInteraction, User user, VelenArguments velenArguments, List<SlashCommandInteractionOption> list, InteractionImmediateResponseBuilder interactionImmediateResponseBuilder) {
+        new MessageBuilder().setEmbed(new Embed().setTitle("Your Majesty wishes to invite me?")
+                .setThumbnail(slashCommandCreateEvent.getApi().getYourself().getAvatar())
+                .setDescription("You can invite me freely by pressing the button below!").build())
+                .addActionRow(Button.link("https://discord.com/api/oauth2/authorize?client_id=786464598835986483&permissions=67488832&scope=bot%20applications.commands",
+                        "Invite me", "\uD83D\uDC9D"))
+                .send(slashCommandInteraction.getChannel().get());
+    }
 }
