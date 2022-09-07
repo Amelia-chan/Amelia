01190bdc99a1ee37155d9e71d657daaefcee90e6
Shindou Mihou <git@mihou.pw>
2022-09-08 01:42:30 +0800

    Removes unused lateinit variables in Configuration.kt

--- src/main/java/pw/mihou/amelia/configuration/Configuration.kt
+++ src/main/java/pw/mihou/amelia/configuration/Configuration.kt
@@ -4,7 +4,5 @@ object Configuration {
 
     lateinit var DISCORD_TOKEN: String
     lateinit var MONGO_URI: String
-    lateinit var WEBSOCKET_AUTHORIZATION: String
-    lateinit var WEBSOCKET_URI: String
 
 }
